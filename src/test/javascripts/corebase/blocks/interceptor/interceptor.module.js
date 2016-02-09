e65a6f3c4cf8f102afc9c459130d0b45cfc2ebf7
Rodrigo Lopes <rodlps22@gmail.com>
2016-02-09 20:55:52 +0000

    version added, remove test cases.

--- src/test/javascripts/corebase/blocks/interceptor/interceptor.module.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('blocks.interceptor', [
-        'blocks.logger'
-    ]);
-
-})();
--- src/test/javascripts/corebase/core/base/constants.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('app.core')
-        .constant('toastr', toastr)
-        .constant('Holder', Holder)
-        .constant('swal',   swal);
-
-})();
