e3da242b21c315cb9a9498e938d445aa1f44f623
Rodrigo Lopes <rodrigo.lopes@fixeads.com>
2016-10-03 16:40:07 +0100

    change helper layout

--- layout/layout.go
+++ layout/layout.go
@@ -1,46 +1,78 @@
 package layout
 
 import (
-	"fmt"
+    "fmt"
 
-	"github.com/rodkranz/ff/terminal"
-	"github.com/rodkranz/ff/core"
-	"github.com/rodkranz/ff/replace"
+    "github.com/rodkranz/ff/terminal"
+    "github.com/rodkranz/ff/core"
+    "github.com/rodkranz/ff/replace"
+    "flag"
+    "os"
+    "path"
 )
 
 type Layout struct {
-	bash *terminal.Bash
+    bash *terminal.Bash
 }
 
 func NewLayout(b *terminal.Bash) *Layout {
-	return &Layout{b}
+    return &Layout{b}
+}
+
+func (l *Layout) GetUsage(app, desc, ver string) func() {
+    return func() {
+
+        fmt.Printf("NAME: \t %v - %v\n", l.bash.Blue(app), l.bash.White(desc))
+        fmt.Printf("USAGE:\t %v %v\n", l.bash.Blue(path.Base(os.Args[0])), l.bash.White("[arguments...]"))
+        fmt.Printf("VER:  \t %v\n", l.bash.Blue(ver))
+
+        fmt.Println("COMMANDS:")
+        flag.VisitAll(func(f *flag.Flag) {
+            extra := (func(extra string) string {
+                if len(extra) == 0 {
+                    return ""
+                }
+
+                return fmt.Sprintf("[default: %v]", l.bash.White(extra))
+            })(f.DefValue)
+
+            space := "\t%v: \t %v %v \n"
+            if len(f.Name) < 7 {
+                space = "\t%v: \t\t %v %v \n"
+            }else if len(f.Name) < 3 {
+                space = "\t%v: +\t\t\t %v %v \n"
+            }
+
+            fmt.Printf(space, l.bash.Green(f.Name), l.bash.Yellow(f.Usage), extra)
+        })
+    }
 }
 
 func (l *Layout) Draw(elms []ff.Element) {
-	for _, elm := range elms {
-		if !elm.Enabled {
-			continue
-		}
+    for _, elm := range elms {
+        if !elm.Enabled {
+            continue
+        }
 
-		l.showFileName(elm)
+        l.showFileName(elm)
 
-		if len(elm.Line) > 0 {
-			l.showComment(elm)
-			l.newLine()
-		}
-	}
+        if len(elm.Line) > 0 {
+            l.showComment(elm)
+            l.newLine()
+        }
+    }
 }
 
 func (l *Layout) DrawWithReplace(rpl replace.Replace, elms []ff.Element, force bool) {
-	for _, elm := range elms {
-		if !elm.Enabled {
-			continue
-		}
+    for _, elm := range elms {
+        if !elm.Enabled {
+            continue
+        }
 
-		l.showFileName(elm)
-		l.askToReplaceComment(elm, rpl, force)
-		l.newLine()
-	}
+        l.showFileName(elm)
+        l.askToReplaceComment(elm, rpl, force)
+        l.newLine()
+    }
 }
 
 func (l *Layout) DrawError(err error) {
@@ -48,85 +80,85 @@ func (l *Layout) DrawError(err error) {
 }
 
 func (l *Layout) showFileName(elm ff.Element) {
-	if len(elm.Line) == 0 {
-		elm.File.Name()
+    if len(elm.Line) == 0 {
+        elm.File.Name()
 
-		typeFile := "file"
-		if elm.File.IsDir() {
-			typeFile = "dir"
-		}
+        typeFile := "file"
+        if elm.File.IsDir() {
+            typeFile = "dir"
+        }
 
-		fmt.Printf("[%v] %v\n",
-			l.bash.White(typeFile),
-			l.bash.Yellow(elm.Path),
-		)
+        fmt.Printf("[%v] %v\n",
+            l.bash.White(typeFile),
+            l.bash.Yellow(elm.Path),
+        )
 
-		return
-	}
+        return
+    }
 
-	fmt.Printf("[%v] %v (lines: %v)\n",
-		l.bash.White("file"),
-		l.bash.Yellow(elm.Path),
-		l.bash.Blue(len(elm.Line)),
-	)
+    fmt.Printf("[%v] %v (lines: %v)\n",
+        l.bash.White("file"),
+        l.bash.Yellow(elm.Path),
+        l.bash.Blue(len(elm.Line)),
+    )
 }
 
 func (l *Layout) showComment(elm ff.Element) {
-	for _, k := range elm.GetComment() {
-		fmt.Printf("[%v] \t%v \n", l.bash.Blue(k), l.bash.ColorWord(elm.Text[k], elm.Line[k]))
-	}
+    for _, k := range elm.GetComment() {
+        fmt.Printf("[%v] \t%v \n", l.bash.Blue(k), l.bash.ColorWord(elm.Text[k], elm.Line[k]))
+    }
 }
 
 func (l *Layout) askToReplaceComment(elm ff.Element, rpl replace.Replace, force bool) {
-	var lines []int
-	for _, k := range elm.GetComment() {
-		line := fmt.Sprintf("[%v] \t%v", l.bash.Blue(k), l.bash.ColorWord(elm.Text[k], elm.Line[k]))
+    var lines []int
+    for _, k := range elm.GetComment() {
+        line := fmt.Sprintf("[%v] \t%v", l.bash.Blue(k), l.bash.ColorWord(elm.Text[k], elm.Line[k]))
 
-		if force {
-			fmt.Printf("%v \t [%v %v %v]\n",
-				line,
-				l.bash.Blue(elm.Text[k]),
-				l.bash.Green("->"),
-				l.bash.Blue(rpl.GetConf().Text),
-			)
+        if force {
+            fmt.Printf("%v \t [%v %v %v]\n",
+                line,
+                l.bash.Blue(elm.Text[k]),
+                l.bash.Green("->"),
+                l.bash.Blue(rpl.GetConf().Text),
+            )
 
-			lines = append(lines, k)
-			continue
-		}
-		c := l.bash.Confirm(fmt.Sprintf("%v [Y/N]", line))
-		if (c) {
-			lines = append(lines, k)
-		}
-	}
+            lines = append(lines, k)
+            continue
+        }
+        c := l.bash.Confirm(fmt.Sprintf("%v [Y/N]", line))
+        if (c) {
+            lines = append(lines, k)
+        }
+    }
 
-	if len(lines) > 0 {
-		rpl.ReplaceLines(elm, lines)
-	}
+    if len(lines) > 0 {
+        rpl.ReplaceLines(elm, lines)
+    }
 }
 
 func (l *Layout) Clear() {
-	fmt.Printf("\r                                  \r")
+    fmt.Printf("\r                                  \r")
 }
 func (l *Layout) ShowWait() {
-	fmt.Printf("Searching please wait... \r")
+    fmt.Printf("Searching please wait... \r")
 }
 
 func (l *Layout) newLine() {
-	fmt.Printf("\n")
+    fmt.Printf("\n")
 }
 
 func (l *Layout) ShowVersion(ver string) {
-	fmt.Printf("The current version of FF is %v\n", ver)
+    fmt.Printf("The current version of FF is %v\n", ver)
 }
 
 func (l *Layout) ShowUpdate(verNew, verOld, link string, has bool) bool {
 
-	if (!has) {
-		fmt.Printf("The application already updated with latest version.\n")
-		return false
-	}
+    if (!has) {
+        fmt.Printf("The application already updated with latest version.\n")
+        return false
+    }
 
-	fmt.Printf(`
+    fmt.Printf(`
 The [FF] application has update available.
    Version Current: %v
  Version Available: %v
@@ -135,6 +167,6 @@ Check Link: %s
 `, verOld, verNew, link)
 
 
-	//return l.bash.Confirm("Would you like to update? [Y/N]");
-	return false
+    //return l.bash.Confirm("Would you like to update? [Y/N]");
+    return false
 }
--- main.go
+++ main.go
@@ -1,161 +1,172 @@
 package main
 
 import (
-	"os"
-	"runtime"
-	"fmt"
-	"flag"
-	"strings"
-	"regexp"
+    "os"
+    "runtime"
+    "fmt"
+    "flag"
+    "strings"
+    "regexp"
 
-	"github.com/fatih/color"
+    "github.com/fatih/color"
 
-	"github.com/rodkranz/ff/core"
-	"github.com/rodkranz/ff/update"
-	"github.com/rodkranz/ff/terminal"
-	"github.com/rodkranz/ff/layout"
-	"github.com/rodkranz/ff/replace"
+    "github.com/rodkranz/ff/core"
+    "github.com/rodkranz/ff/update"
+    "github.com/rodkranz/ff/terminal"
+    "github.com/rodkranz/ff/layout"
+    "github.com/rodkranz/ff/replace"
 )
 
-const VER = "1.2.0"
+const (
+    APP string = "FF"
+    DESC string = "Find Text Or File."
+    VER string = "1.2.0"
+)
 
 var conf = &configuration{}
 
 func init() {
+    // configuration of update
+    conf.upConfig = update.Config{
+        VER,
+        `https://github.com/rodkranz/ff/releases`,
+        `href=\"/rodkranz/ff/releases/tag/[ANY]\"`,
+    }
 
-	// configuration of update
-	conf.upConfig = update.Config{
-		VER,
-		`https://github.com/rodkranz/ff/releases`,
-		`href=\"/rodkranz/ff/releases/tag/[ANY]\"`,
-	}
+    // General
+    flag.IntVar(&conf.CPUNum, "cpu", runtime.NumCPU(), fmt.Sprintf("Number of CPU you have %d available", runtime.NumCPU()))
+    flag.BoolVar(&conf.force, "force", false, "Replace all result without ask.")
+    //flag.IntVar(&conf.reach, 	"r", 10, "Range around of the word")
 
-	// General
-	flag.IntVar(&conf.CPUNum, 	"cpu", runtime.NumCPU(), fmt.Sprintf("Number of CPU you have %d available", runtime.NumCPU()))
-	flag.BoolVar(&conf.force,  	"force", 	false, "Replace all result without ask.")
-	//flag.IntVar(&conf.reach, 	"r", 10, "Range around of the word")
+    // FF Config
+    flag.StringVar(&conf.ffConfig.Text, "t", "", "Text that you are looking for")
+    flag.StringVar(&conf.ffConfig.FilterFile, "f", "", "Filter by file name")
+    flag.StringVar(&conf.ffConfig.Directory, "d", "./", "Set a specify directory")
+    flag.BoolVar(&conf.ffConfig.CaseInsensitive, "cis", false, "Search text case insensitive")
 
-	// FF Config
-	flag.StringVar(&conf.ffConfig.Text, 			"t", "", "Text ffConfig")
-	flag.StringVar(&conf.ffConfig.FilterFile, 		"f", "", "Filter by file name")
-	flag.StringVar(&conf.ffConfig.Directory, 		"d", "./", "Directory ffConfig")
-	flag.BoolVar(&conf.ffConfig.CaseInsensitive, 	"cis", false, "Search text case insensitive")
+    // Replace Config
+    flag.StringVar(&conf.rpConfig.Text, "replace", "", "Replace result to text")
 
-	// Replace Config
-	flag.StringVar(&conf.rpConfig.Text, "replace",  "", "Replace result to text")
+    // Update Config
+    flag.BoolVar(&conf.showVersion, "ver", false, "Show the version")
+    flag.BoolVar(&conf.upCheck, "up", false, "Check update")
 
-	// Update Config
-	flag.BoolVar(&conf.showVersion, "ver", false, "Show the version")
-	flag.BoolVar(&conf.upCheck, 	"up", 		false, "Check update")
+    // Output config
+    flag.BoolVar(&color.NoColor, "-no-color", false, "Disable color output")
 
-	// Output config
-	flag.BoolVar(&color.NoColor, 	"-no-color", false, "Disable color output")
+    // FF with regex
+    var withRegex bool
+    flag.BoolVar(&withRegex, "reg", false, "Search by this Regex")
 
-	// FF with regex
-	var withRegex bool
-	flag.BoolVar(&withRegex, 	"reg", false, "Search by this Regex")
+    // exclude files or folder with this name by default.
+    exclude := *flag.String("-exclude-dir", ".bzr,CVS,.git,.hg,.svn", "Exclude dir from reader")
 
-	// exclude files or folder with this name by default.
-	exclude := *flag.String("-exclude-dir", ".bzr,CVS,.git,.hg,.svn", "Exclude dir from reader")
+    // Allow use the CPUs available
+    runtime.GOMAXPROCS(conf.CPUNum)
 
-	flag.Parse()
+    //New Output mode
+    bash := terminal.NewBash()
 
-	if len(exclude) > 0 {
-		conf.ffConfig.ExcludeFiles = strings.Split(exclude, ",")
-	}
+    //Layout that will draw the contents
+    conf.draw = layout.NewLayout(bash)
 
-	if withRegex { // if has regex
-		conf.ffConfig.Regex = regexp.MustCompile(conf.ffConfig.Text)
-	}
+    // Show Usage Pretty
+    flag.Usage = conf.draw.GetUsage(APP, DESC, VER)
 
-	// if has replace
-	conf.rpConfig.WithReplace = false
-	if(len(conf.rpConfig.Text) != 0) {
-		conf.rpConfig.WithReplace = true
-	}
+    // Parse parameters from console.
+    flag.Parse()
 
-	if conf.CPUNum > runtime.NumCPU() {
-		conf.CPUNum = runtime.NumCPU()
-	}
+    if len(exclude) > 0 {
+        conf.ffConfig.ExcludeFiles = strings.Split(exclude, ",")
+    }
 
-	// Check if has parameters
-	if flag.NArg() == 1 {
-		if par := flag.Arg(0); par[0] != 45 {
-			conf.ffConfig.Text = flag.Arg(0)
-		}
-	}
+    if withRegex {
+        // if has regex
+        conf.ffConfig.Regex = regexp.MustCompile(conf.ffConfig.Text)
+    }
+
+    // if has replace
+    conf.rpConfig.WithReplace = false
+    if (len(conf.rpConfig.Text) != 0) {
+        conf.rpConfig.WithReplace = true
+    }
+
+    if conf.CPUNum > runtime.NumCPU() {
+        conf.CPUNum = runtime.NumCPU()
+    }
+
+    // Check if has parameters
+    if flag.NArg() == 1 {
+        if par := flag.Arg(0); par[0] != 45 {
+            conf.ffConfig.Text = flag.Arg(0)
+        }
+    }
 }
 
 func main() {
-	// Allow use the CPUs available
-	runtime.GOMAXPROCS(conf.CPUNum)
+    draw := conf.draw
 
-	//New Output mode
-	bash   := terminal.NewBash()
+    if conf.showVersion || conf.upCheck {
 
-	//Layout that will draw the contents
-	layout := layout.NewLayout(bash)
+        // show the current version of program
+        if conf.showVersion {
+            draw.ShowVersion(VER)
+            os.Exit(0)
+        }
 
-	if conf.showVersion || conf.upCheck {
+        // create a instance of update
+        up := update.NewUpdate(conf.upConfig)
 
-		// show the current vertion of program
-		if conf.showVersion {
-			layout.ShowVersion(VER)
-			os.Exit(0)
-		}
+        // Check if has update and inform layout to do what next.
+        if draw.ShowUpdate(up.Check()) {
+            // if has update.
+            up.Update();
+        }
 
-		// create a instance of update
-		up := update.NewUpdate(conf.upConfig)
+        // finalize the application correctly
+        os.Exit(0)
+    }
 
-		// Check if has update and inform layout to do what next.
-		if layout.ShowUpdate(up.Check()) {
-			// if has update.
-			up.Update();
-		}
+    // Show wait text...
+    draw.ShowWait()
 
-		// finalize the application correctly
-		os.Exit(0)
-	}
+    // create a virtual storage
+    finder := ff.NewFinder(conf.ffConfig)
 
-	// Show wait text...
-	layout.ShowWait()
+    // search the files/text
+    finder.FindFiles(conf.ffConfig.Directory)
+    finder.FindText(conf.ffConfig.Text)
 
-	// create a virtual storage
-	finder := ff.NewFinder(conf.ffConfig)
+    result, err := finder.GetResult()
+    if err != nil {
+        // Draw error
+        draw.DrawError(err)
+        // finalize the application with error.
+        os.Exit(1)
+    }
 
-	// search the files/text
-	finder.FindFiles(conf.ffConfig.Directory)
-	finder.FindText(conf.ffConfig.Text)
+    draw.Clear()
+    if conf.rpConfig.WithReplace {
+        // Draw result with Replace
+        rpl := replace.NewReplace(conf.rpConfig)
+        draw.DrawWithReplace(*rpl, result, conf.force)
+    } else {
+        // Draw results
+        draw.Draw(result)
+    }
 
-	result, err := finder.GetResult()
-	if err != nil {
-		// Draw error
-		layout.DrawError(err)
-		// finalize the application with error.
-		os.Exit(1)
-	}
-
-	layout.Clear()
-	if conf.rpConfig.WithReplace {
-		// Draw result with Replace
-		rpl := replace.NewReplace(conf.rpConfig)
-		layout.DrawWithReplace(*rpl, result, conf.force)
-	} else {
-		// Draw results
-		layout.Draw(result)
-	}
-
-	// finalize the application correctly
-	os.Exit(0)
+    // finalize the application correctly
+    os.Exit(0)
 }
 
 type configuration struct {
-	rpConfig 	replace.Config
-	ffConfig    ff.Config
-	upConfig    update.Config
+    rpConfig    replace.Config
+    ffConfig    ff.Config
+    upConfig    update.Config
+    draw        *layout.Layout
 
-	force		bool
-	showVersion bool
-	upCheck     bool
-	CPUNum      int
+    force       bool
+    showVersion bool
+    upCheck     bool
+    CPUNum      int
 }
